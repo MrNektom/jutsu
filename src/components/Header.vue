52c778ff2bb1560395fee448e4ffb12b818066d9
Igor GVNB <kiraritypical@gmail.com>
2023-03-25 19:58:08 +0300

    Изменены стили для мобильной версии

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -3,25 +3,25 @@
         <div class="header-content">
             <h1 class="header-title">{{ title }}</h1>
             <nav class="nav">
-                <div class="anime-nav">
+                <div class="anime-nav" :id="$route.path === '/' ? 'active' : ''">
                     <router-link class="link-nav" to="/">
                         <img :src="animeSrc" alt="" class="anime-icon">
                         <h3>Аниме</h3>
                     </router-link>
                 </div>
-                <div class="forum-nav">
+                <div class="forum-nav" :id="$route.path === '/forum' ? 'active' : ''">
                     <router-link class="link-nav" to="/forum">
                         <img :src="forumSrc" alt="" class="forum-icon">
                         <h3>Форум</h3>
                     </router-link>
                 </div>
-                <div class="pedia-nav">
+                <div class="pedia-nav" :id="$route.path === '/pedia' ? 'active' : ''">
                     <router-link class="link-nav" to="/pedia">
                         <img :src="pediaSrc" alt="" class="pedia-icon">
                         <h3>Нарутопедия</h3>
                     </router-link>
                 </div>
-                <div class="profile-nav">
+                <div class="profile-nav" :id="$route.path === '/profile' ? 'active' : ''">
                     <router-link class="link-nav" to="/profile">
                         <img :src="profileSrc" alt="" class="profile-icon">
                         <h3>Профиль</h3>
